376c2e7629a0b1ef2dfa3a081db928463d78ec00
Oksana <skelf222@yandex.ru>
2022-12-16 16:29:58 +0300

    1 балл

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'no-console': 0,
+    'no-tabs': 0,
+    'prefer-destructuring': 0,
+    'no-alert': 0,
+    'consistent-return': 0,
+  },
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("work");
+const $wr = document.querySelector('[data-wr]')
+
+const getCatHTML = (cat) => {
+	return `
+		<div class="card mb-4 mx-4" style="width: 18rem">
+		<img src="${cat.image}" class="card-img-top" alt="${cat.name}" />
+		<div class="card-body">
+			<h5 class="card-title">${cat.name}</h5>
+			<p class="card-text">
+				${cat.description}
+			</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	</div>
+	`
+}
+
+fetch('https://cats.petiteweb.dev/api/single/Oksana1047/show/')
+	.then((res) => res.json())
+	.then((data) => {
+
+		$wr.insertAdjacentHTML('afterbegin', data.map(cat => getCatHTML(cat)).join(''))
+
+		console.log({ data })
+
+
+	})
